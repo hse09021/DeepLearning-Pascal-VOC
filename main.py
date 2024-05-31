af91b696a15e2248c264d63976729d277d18c92c
kimgeonsu <kundoo0412@gmail.com>
2024-05-31 12:25:24 +0900

    update resnet

--- main.py
+++ main.py
@@ -8,36 +8,37 @@ import torch
 import torchvision
 from torchvision import transforms
 
-from nets.nn import resnet50, resnet152
+from nets.nn import resnet50, resnet152, resnetNew
 from utils.loss import yoloLoss
 from utils.dataset import Dataset
 
 import argparse
 import re
 
+
 def main(args):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    
+
     root = args.data_dir
     num_epochs = args.epoch
     batch_size = args.batch_size
     learning_rate = args.lr
-    
+
     seed = 42
     np.random.seed(seed)
     torch.manual_seed(seed)
 
     # net = resnet50()
-    net = resnet152()
+    # net = resnet152()
+    net = resnetNew()
 
-    
-    if(args.pre_weights != None):
+    if (args.pre_weights != None):
         pattern = 'yolov1_([0-9]+)'
         strs = args.pre_weights.split('.')[-2]
         f_name = strs.split('/')[-1]
-        epoch_str = re.search(pattern,f_name).group(1)
+        epoch_str = re.search(pattern, f_name).group(1)
         epoch_start = int(epoch_str) + 1
-        net.load_state_dict( \
+        net.load_state_dict(
             torch.load(f'./weights/{args.pre_weights}')['state_dict'])
     else:
         epoch_start = 1
@@ -45,7 +46,7 @@ def main(args):
         resnet = torchvision.models.resnet152(pretrained=True)
 
         new_state_dict = resnet.state_dict()
-    
+
         net_dict = net.state_dict()
         for k in new_state_dict.keys():
             if k in net_dict.keys() and not k.startswith('fc'):
@@ -60,7 +61,7 @@ def main(args):
     if torch.cuda.device_count() > 1:
         net = torch.nn.DataParallel(net)
 
-    #summary(net,input_size=(3,448,448))
+    # summary(net,input_size=(3,448,448))
     # different learning rate
 
     net.train()
@@ -73,25 +74,27 @@ def main(args):
         else:
             params += [{'params': [value], 'lr': learning_rate}]
 
-    optimizer = torch.optim.SGD(params, lr=learning_rate, momentum=0.9, weight_decay=5e-4)
+    optimizer = torch.optim.SGD(
+        params, lr=learning_rate, momentum=0.9, weight_decay=5e-4)
     # optimizer = torch.optim.RMSprop(params, lr=learning_rate, alpha=0.99, eps=1e-08, weight_decay=5e-4, momentum=0.9)
 
     tr = [
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[
+                             0.229, 0.224, 0.225])
     ]
 
     with open('./Dataset/train.txt') as f:
         train_names = f.readlines()
     train_dataset = Dataset(root, train_names, train=True, transform=tr)
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True,
-                                            num_workers=os.cpu_count())
+                                               num_workers=os.cpu_count())
 
     with open('./Dataset/test.txt') as f:
         test_names = f.readlines()
     test_dataset = Dataset(root, test_names, train=False, transform=tr)
     test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size // 2, shuffle=False,
-                                            num_workers=os.cpu_count())
+                                              num_workers=os.cpu_count())
 
     print(f'NUMBER OF DATA SAMPLES: {len(train_dataset)}')
     print(f'BATCH SIZE: {batch_size}')
@@ -101,8 +104,7 @@ def main(args):
     patience = 5
     count = 0
 
-
-    for epoch in range(epoch_start,num_epochs):
+    for epoch in range(epoch_start, num_epochs):
         net.train()
 
         if epoch == 30:
@@ -115,13 +117,14 @@ def main(args):
         # training
         total_loss = 0.
         print(('\n' + '%10s' * 3) % ('epoch', 'loss', 'gpu'))
-        progress_bar = tqdm.tqdm(enumerate(train_loader), total=len(train_loader))
+        progress_bar = tqdm.tqdm(
+            enumerate(train_loader), total=len(train_loader))
         for i, (images, target) in progress_bar:
             images = images.to(device)
             target = target.to(device)
 
             pred = net(images)
-            
+
             optimizer.zero_grad()
             loss = criterion(pred, target.float())
 
@@ -129,16 +132,18 @@ def main(args):
             optimizer.step()
 
             total_loss += loss.item()
-            mem = '%.3gG' % (torch.cuda.memory_reserved() / 1E9 if torch.cuda.is_available() else 0)
-            s = ('%10s' + '%10.4g' + '%10s') % ('%g/%g' % (epoch, num_epochs), total_loss / (i + 1), mem)
+            mem = '%.3gG' % (torch.cuda.memory_reserved() /
+                             1E9 if torch.cuda.is_available() else 0)
+            s = ('%10s' + '%10.4g' + '%10s') % ('%g/%g' %
+                                                (epoch, num_epochs), total_loss / (i + 1), mem)
             progress_bar.set_description(s)
-        
-        
+
         # validation
         validation_loss = 0.0
         net.eval()
         with torch.no_grad():
-            progress_bar = tqdm.tqdm(enumerate(test_loader), total=len(test_loader))
+            progress_bar = tqdm.tqdm(
+                enumerate(test_loader), total=len(test_loader))
             for i, (images, target) in progress_bar:
                 images = images.to(device)
                 target = target.to(device)
@@ -146,7 +151,7 @@ def main(args):
                 prediction = net(images)
                 loss = criterion(prediction, target)
                 validation_loss += loss.data
-            
+
         validation_loss /= len(test_loader)
         print(f'Validation_Loss:{validation_loss:07.3}')
 
@@ -160,8 +165,7 @@ def main(args):
                 print(f"Early stopping at epoch={epoch}")
                 break
 
-        
-        #if epoch % 5:
+        # if epoch % 5:
         #    save = {'state_dict': net.state_dict()}
         #    torch.save(save, f'./weights/yolov1_{epoch+1:04d}.pth')
         save = {'state_dict': net.state_dict()}
@@ -169,6 +173,7 @@ def main(args):
 
     save = {'state_dict': net.state_dict()}
     torch.save(save, './weights/yolov1_final.pth')
+
 
 if __name__ == '__main__':
 
@@ -182,9 +187,8 @@ if __name__ == '__main__':
     parser.add_argument("--save_dir", type=str, default="./weights")
     parser.add_argument("--img_size", type=int, default=448)
     args = parser.parse_args()
-    
+
     args.pre_weights = 'yolov1_0010.pth'
     main(args)
-
 
     # python3 main.py --epoch 100
--- nets/nn.py
+++ nets/nn.py
@@ -94,15 +94,18 @@ class DetNet(nn.Module):
         super(DetNet, self).__init__()
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=2, bias=False, dilation=2)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
+                               stride=stride, padding=2, bias=False, dilation=2)
         self.bn2 = nn.BatchNorm2d(planes)
-        self.conv3 = nn.Conv2d(planes, self.expansion * planes, kernel_size=1, bias=False)
+        self.conv3 = nn.Conv2d(planes, self.expansion *
+                               planes, kernel_size=1, bias=False)
         self.bn3 = nn.BatchNorm2d(self.expansion * planes)
 
         self.downsample = nn.Sequential()
         if stride != 1 or in_planes != self.expansion * planes or block_type == 'B':
             self.downsample = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
+                nn.Conv2d(in_planes, self.expansion * planes,
+                          kernel_size=1, stride=stride, bias=False),
                 nn.BatchNorm2d(self.expansion * planes)
             )
 
@@ -116,11 +119,11 @@ class DetNet(nn.Module):
 
 
 class ResNet(nn.Module):
-
     def __init__(self, block, layers, num_classes=1000):
         self.in_planes = 64
         super(ResNet, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7,
+                               stride=2, padding=3, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
         self.relu = nn.ReLU(inplace=True)
 
@@ -134,7 +137,8 @@ class ResNet(nn.Module):
         self.layer5 = self._make_detnet_layer(in_channels=2048)
         # self.avgpool = nn.AvgPool2d(14) #fit 448 input size
         # self.fc = nn.Linear(512 * block.expansion, num_classes)
-        self.conv_end = nn.Conv2d(256, 30, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv_end = nn.Conv2d(
+            256, 30, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn_end = nn.BatchNorm2d(30)
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -148,10 +152,11 @@ class ResNet(nn.Module):
         downsample = None
         if stride != 1 or self.in_planes != planes * block.expansion:
             downsample = nn.Sequential(
-                nn.Conv2d(self.in_planes, planes * block.expansion, kernel_size=1, stride=stride, bias=False),
+                nn.Conv2d(self.in_planes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
                 nn.BatchNorm2d(planes * block.expansion),
             )
-        #[3, 4, 6, 3]
+        # [3, 4, 6, 3]
         layers = [block(self.in_planes, planes, stride, downsample)]
         self.in_planes = planes * block.expansion
         for i in range(1, blocks):
@@ -190,17 +195,114 @@ class ResNet(nn.Module):
         return x
 
 
+class ResNetNew(nn.Module):
+    def __init__(self, block, layers, num_classes=1000):
+        self.in_planes = 64
+        super(ResNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7,
+                               stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        self.layer5 = self._make_detnet_layer(in_channels=2048)
+        self.layer6 = self._make_detection_layer()  # 새로운 객체 탐지 레이어 추가
+
+        self.conv_end = nn.Conv2d(
+            256, 30, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn_end = nn.BatchNorm2d(30)
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.in_planes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.in_planes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+        layers = [block(self.in_planes, planes, stride, downsample)]
+        self.in_planes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.in_planes, planes))
+
+        return nn.Sequential(*layers)
+
+    def _make_detnet_layer(self, in_channels):
+        layers = [
+            DetNet(in_planes=in_channels, planes=256, block_type='B'),
+            DetNet(in_planes=256, planes=256, block_type='A'),
+            DetNet(in_planes=256, planes=256, block_type='A')
+        ]
+        return nn.Sequential(*layers)
+
+    def _make_detection_layer(self):
+        # 객체 탐지를 위한 추가 레이어
+        return nn.Sequential(
+            nn.Conv2d(256, 512, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 1024, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(1024),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(1024, num_classes, kernel_size=1, bias=False),
+            nn.Sigmoid()  # 클래스 확률을 출력하기 위해 Sigmoid 사용
+        )
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.layer5(x)
+        x = self.layer6(x)  # 새로운 객체 탐지 레이어를 통과
+
+        x = self.conv_end(x)
+        x = self.bn_end(x)
+        x = torch.sigmoid(x)
+        x = x.permute(0, 2, 3, 1)  # (-1,14,14,30)
+
+        return x
+
+
 # resnet50
 def resnet50(pretrained=False, **kwargs):
     model_ = ResNet(Bottleneck, [3, 4, 6, 3], **kwargs)
     if pretrained:
-        model_.load_state_dict(model_zoo.load_url('https://download.pytorch.org/models/resnet50-19c8e357.pth'))
+        model_.load_state_dict(model_zoo.load_url(
+            'https://download.pytorch.org/models/resnet50-19c8e357.pth'))
     return model_
+
 
 def resnet152(pretrained=False, **kwargs):
     model_ = ResNet(Bottleneck, [3, 4, 6, 3], **kwargs)
     if pretrained:
-        model_.load_state_dict(model_zoo.load_url('https://download.pytorch.org/models/resnet152-394f9c45.pth'))
+        model_.load_state_dict(model_zoo.load_url(
+            'https://download.pytorch.org/models/resnet152-394f9c45.pth'))
+    return model_
+
+
+def resnetNew(pretrained=False, **kwargs):
+    model_ = ResNetNew(Bottleneck, [3, 4, 6, 3], **kwargs)
+    if pretrained:
+        model_.load_state_dict(model_zoo.load_url(
+            'https://download.pytorch.org/models/resnet152-394f9c45.pth'))
     return model_
 
 

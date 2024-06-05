0215d0e868149c3250d7499df72e859b243794b5
taegyun <ypp1900@naver.com>
2024-06-05 18:14:29 +0900

    convert to origin

--- main.py
+++ main.py
@@ -1,76 +1,47 @@
 import os
 import tqdm
 import numpy as np
-import matplotlib.pyplot as plt
 from torchsummary import summary
 
 import torch
 import torchvision
 from torchvision import transforms
 
-from nets.nn import resnet50, resnet152, vit
+from nets.nn import resnet50, resnet50FPN
 from utils.loss import yoloLoss
 from utils.dataset import Dataset
-from collections import Counter
+
 import argparse
 import re
 
-
-def ensemble_predict(models, images):
-    # 모든 모델의 예측 결과를 저장할 텐서 초기화
-    predictions = [model(images) for model in models]
-    # 모든 예측 결과의 평균을 계산
-    avg_predictions = torch.mean(torch.stack(predictions), dim=0)
-    return avg_predictions
-
-
-def voting_ensemble(models, images):
-    # 각 모델의 예측 결과를 저장
-    predictions = [model(images).max(1)[1] for model in models]
-    predictions = torch.stack(predictions)
-
-    # 투표 진행
-    result = []
-    for i in range(predictions.shape[1]):  # 각 샘플에 대해
-        # 각 샘플에 대한 예측을 모아 리스트로 만듭니다.
-        sample_predictions = predictions[:, i].tolist()
-        # 각 샘플에 대한 투표 결과를 집계합니다.
-        vote_result = Counter(sample_predictions)
-        # 가장 많이 나온 예측 선택
-        most_common = vote_result.most_common(1)[0][0]
-        result.append(most_common)
-    return torch.tensor(result, device=predictions.device)
-
-
 def main(args):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
+    
     root = args.data_dir
     num_epochs = args.epoch
     batch_size = args.batch_size
     learning_rate = args.lr
-
+    
     seed = 42
     np.random.seed(seed)
     torch.manual_seed(seed)
 
-    net = vit(pretrained=True)
-
-    if (args.pre_weights != None):
+    # net = resnet50()
+    net = resnet50FPN()
+    
+    if(args.pre_weights != None):
         pattern = 'yolov1_([0-9]+)'
         strs = args.pre_weights.split('.')[-2]
         f_name = strs.split('/')[-1]
-        epoch_str = re.search(pattern, f_name).group(1)
+        epoch_str = re.search(pattern,f_name).group(1)
         epoch_start = int(epoch_str) + 1
-        # net1.load_state_dict(
-        #     torch.load(f'./weights/{args.pre_weights}')['state_dict'])
+        net.load_state_dict( \
+            torch.load(f'./weights/{args.pre_weights}')['state_dict'])
     else:
         epoch_start = 1
-        # resnet = torchvision.models.resnet50(pretrained=True)
-        resnet = torchvision.models.resnet152(pretrained=True)
-
+        resnet = torchvision.models.resnet50(pretrained=True)
         new_state_dict = resnet.state_dict()
-
+    
         net_dict = net.state_dict()
         for k in new_state_dict.keys():
             if k in net_dict.keys() and not k.startswith('fc'):
@@ -85,7 +56,7 @@ def main(args):
     if torch.cuda.device_count() > 1:
         net = torch.nn.DataParallel(net)
 
-    # summary(net,input_size=(3,448,448))
+    #summary(net,input_size=(3,448,448))
     # different learning rate
 
     net.train()
@@ -98,37 +69,26 @@ def main(args):
         else:
             params += [{'params': [value], 'lr': learning_rate}]
 
-    optimizer = torch.optim.SGD(
-        params, lr=learning_rate, momentum=0.9, weight_decay=5e-4)
-    # optimizer = torch.optim.RMSprop(params, lr=learning_rate, alpha=0.99, eps=1e-08, weight_decay=5e-4, momentum=0.9)
+    optimizer = torch.optim.SGD(params, lr=learning_rate, momentum=0.9, weight_decay=5e-4)
 
-    tr = [
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[
-                             0.229, 0.224, 0.225])
-    ]
+    #optimizer = torch.optim.SGD(net.parameters(), lr=learning_rate, momentum=0.9, weight_decay=5e-4)
 
     with open('./Dataset/train.txt') as f:
         train_names = f.readlines()
-    train_dataset = Dataset(root, train_names, train=True, transform=tr)
+    train_dataset = Dataset(root, train_names, train=True, transform=[transforms.ToTensor()])
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True,
-                                               num_workers=os.cpu_count())
+                                            num_workers=os.cpu_count())
 
     with open('./Dataset/test.txt') as f:
         test_names = f.readlines()
-    test_dataset = Dataset(root, test_names, train=False, transform=tr)
+    test_dataset = Dataset(root, test_names, train=False, transform=[transforms.ToTensor()])
     test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size // 2, shuffle=False,
-                                              num_workers=os.cpu_count())
+                                            num_workers=os.cpu_count())
 
     print(f'NUMBER OF DATA SAMPLES: {len(train_dataset)}')
     print(f'BATCH SIZE: {batch_size}')
 
-    """ early stopping 추가 """
-    best_validation_loss = float("inf")
-    patience = 5
-    count = 0
-
-    for epoch in range(epoch_start, num_epochs):
+    for epoch in range(epoch_start,num_epochs):
         net.train()
 
         if epoch == 30:
@@ -141,33 +101,30 @@ def main(args):
         # training
         total_loss = 0.
         print(('\n' + '%10s' * 3) % ('epoch', 'loss', 'gpu'))
-        progress_bar = tqdm.tqdm(
-            enumerate(train_loader), total=len(train_loader))
+        progress_bar = tqdm.tqdm(enumerate(train_loader), total=len(train_loader))
         for i, (images, target) in progress_bar:
-            images = torch.nn.functional.interpolate(
-                images, size=(224, 224)).to(device)
+            images = images.to(device)
             target = target.to(device)
 
             pred = net(images)
-
+            
             optimizer.zero_grad()
             loss = criterion(pred, target.float())
+
             loss.backward()
             optimizer.step()
 
             total_loss += loss.item()
-            mem = '%.3gG' % (torch.cuda.memory_reserved() /
-                             1E9 if torch.cuda.is_available() else 0)
-            s = ('%10s' + '%10.4g' + '%10s') % ('%g/%g' %
-                                                (epoch, num_epochs), total_loss / (i + 1), mem)
+            mem = '%.3gG' % (torch.cuda.memory_reserved() / 1E9 if torch.cuda.is_available() else 0)
+            s = ('%10s' + '%10.4g' + '%10s') % ('%g/%g' % (epoch, num_epochs), total_loss / (i + 1), mem)
             progress_bar.set_description(s)
-
+        
+        
         # validation
         validation_loss = 0.0
         net.eval()
         with torch.no_grad():
-            progress_bar = tqdm.tqdm(
-                enumerate(test_loader), total=len(test_loader))
+            progress_bar = tqdm.tqdm(enumerate(test_loader), total=len(test_loader))
             for i, (images, target) in progress_bar:
                 images = images.to(device)
                 target = target.to(device)
@@ -175,21 +132,11 @@ def main(args):
                 prediction = net(images)
                 loss = criterion(prediction, target)
                 validation_loss += loss.data
-
+            
         validation_loss /= len(test_loader)
         print(f'Validation_Loss:{validation_loss:07.3}')
-
-        """ early stopping 추가 """
-        if validation_loss < best_validation_loss:
-            best_validation_loss = validation_loss
-            count = 0
-        else:
-            count += 1
-            if count >= patience:
-                print(f"Early stopping at epoch={epoch}")
-                break
-
-        # if epoch % 5:
+        
+        #if epoch % 5:
         #    save = {'state_dict': net.state_dict()}
         #    torch.save(save, f'./weights/yolov1_{epoch+1:04d}.pth')
         save = {'state_dict': net.state_dict()}
@@ -198,21 +145,18 @@ def main(args):
     save = {'state_dict': net.state_dict()}
     torch.save(save, './weights/yolov1_final.pth')
 
-
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--batch_size", type=int, default=32)
-    parser.add_argument("--epoch", type=int, default=50)
+    parser.add_argument("--batch_size", type=int, default=8)
+    parser.add_argument("--epoch", type=int, default=30)
     parser.add_argument("--lr", type=float, default=0.001)
     parser.add_argument("--data_dir", type=str, default='./Dataset')
     parser.add_argument("--pre_weights", type=str, help="pretrained weight")
     parser.add_argument("--save_dir", type=str, default="./weights")
     parser.add_argument("--img_size", type=int, default=448)
     args = parser.parse_args()
-
+    
     args.pre_weights = 'yolov1_0010.pth'
     main(args)
-
-    # python3 main.py --epoch 100

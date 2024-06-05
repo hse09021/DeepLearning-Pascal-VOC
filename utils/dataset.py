056105ec55b931433ac7723fb98c6667b9b71232
taegyun <ypp1900@naver.com>
2024-06-05 17:45:47 +0900

    convert to origin

--- utils/dataset.py
+++ utils/dataset.py
@@ -55,18 +55,7 @@ class Dataset(data.Dataset):
             img = self.RandomSaturation(img)
             img, boxes, labels = self.randomShift(img, boxes, labels)
             img, boxes, labels = self.randomCrop(img, boxes, labels)
-            
-            """ 추가된 데이터 증강 기법들 """
-            img, boxes = self.randomRotate(img, boxes)
-            img = self.addGaussianNoise(img)
 
-            """ 바운딩 박스를 이미지 크기에 맞게 조정 """
-            h, w, _ = img.shape
-            boxes = self.clamp_boxes(boxes, w, h)
-
-            """ 모자이크 기법 추가 """
-            # img, boxes, labels = self.mosaic(img, boxes, labels)
-            
         # # debug
         # box_show = boxes.numpy().reshape(-1)
         # print(box_show)
@@ -85,7 +74,6 @@ class Dataset(data.Dataset):
         img = self.BGR2RGB(img)
         img = self.subMean(img, self.mean)
         img = cv2.resize(img, (self.image_size, self.image_size))
-
         target = self.encoder(boxes, labels)  # 14x14x30
         for t in self.transform:
             img = t(img)
@@ -129,99 +117,6 @@ class Dataset(data.Dataset):
 
     def HSV2BGR(self, img):
         return cv2.cvtColor(img, cv2.COLOR_HSV2BGR)
-    
-    """ 바운딩 박스 좌표의 유효성 확인을 위한 클램프 박스 추가 """
-    @staticmethod
-    def clamp_boxes(boxes, width, height):
-        boxes[:, 0].clamp_(0, width)
-        boxes[:, 1].clamp_(0, height)
-        boxes[:, 2].clamp_(0, width)
-        boxes[:, 3].clamp_(0, height)
-        return boxes
-    
-    """ 랜덤 회전, 가우시안 노이즈 추가 """
-    def randomRotate(self, bgr, boxes, angle_range=(-10, 10)):
-        if random.random() < 0.5:
-            angle = random.uniform(*angle_range)
-            height, width = bgr.shape[:2]
-            center = (width // 2, height // 2)
-            matrix = cv2.getRotationMatrix2D(center, angle, 1.0)
-            cos = np.abs(matrix[0, 0])
-            sin = np.abs(matrix[0, 1])
-            new_width = int((height * sin) + (width * cos))
-            new_height = int((height * cos) + (width * sin))
-            matrix[0, 2] += (new_width / 2) - center[0]
-            matrix[1, 2] += (new_height / 2) - center[1]
-            rotated_img = cv2.warpAffine(bgr, matrix, (new_width, new_height))
-            
-            # 바운딩 박스 좌표 회전
-            boxes_np = boxes.numpy()
-            for i in range(len(boxes_np)):
-                xmin, ymin, xmax, ymax = boxes_np[i]
-                points = np.array([[xmin, ymin], [xmax, ymin], [xmin, ymax], [xmax, ymax]])
-                ones = np.ones(shape=(len(points), 1))
-                points_ones = np.hstack([points, ones])
-                rotated_points = matrix @ points_ones.T
-                xmin, ymin = rotated_points[:2].min(axis=1)
-                xmax, ymax = rotated_points[:2].max(axis=1)
-                boxes_np[i] = [xmin, ymin, xmax, ymax]
-            boxes = torch.Tensor(boxes_np)
-            return rotated_img, boxes
-        return bgr, boxes
-
-    def addGaussianNoise(self, bgr, mean=0, std=0.01):
-        if random.random() < 0.5:
-            noise = np.random.normal(mean, std, bgr.shape).astype(bgr.dtype)
-            bgr = cv2.add(bgr, noise)
-        return bgr
-    
-    """ 모자이크 기법 """
-    def mosaic(self, img, boxes, labels, ratio=0.2):
-        """
-        이미지에 모자이크를 적용하여 데이터 증강을 수행하는 함수
-
-        Args:
-            img (numpy.ndarray): 원본 이미지
-            boxes (numpy.ndarray): 바운딩 박스들의 좌표 [x_min, y_min, x_max, y_max]
-            labels (numpy.ndarray): 바운딩 박스들에 대한 라벨
-            ratio (float): 모자이크 비율, 기본값은 0.2
-
-        Returns:
-            numpy.ndarray: 모자이크가 적용된 이미지
-            numpy.ndarray: 모자이크가 적용된 바운딩 박스들의 좌표
-            numpy.ndarray: 모자이크가 적용된 바운딩 박스들의 라벨
-        """
-        h, w, _ = img.shape
-        new_img = img.copy()
-
-        # 모자이크를 적용할 영역의 크기 계산
-        mosaiced_h = int(h * ratio)
-        mosaiced_w = int(w * ratio)
-
-        # 랜덤하게 모자이크 적용할 영역 선택
-        y_offset = np.random.randint(0, h - mosaiced_h + 1)
-        x_offset = np.random.randint(0, w - mosaiced_w + 1)
-
-        # 모자이크 적용
-        mosaic_area = img[y_offset:y_offset + mosaiced_h, x_offset:x_offset + mosaiced_w]
-        mosaic_area = cv2.resize(mosaic_area, (mosaiced_w, mosaiced_h), interpolation=cv2.INTER_NEAREST)
-        new_img[y_offset:y_offset + mosaiced_h, x_offset:x_offset + mosaiced_w] = mosaic_area
-
-        # 모자이크 영역에 속하는 바운딩 박스들의 좌표 및 라벨을 업데이트
-        for i in range(len(boxes)):
-            box = boxes[i]
-            if box[0] >= x_offset and box[2] <= x_offset + mosaiced_w and box[1] >= y_offset and box[3] <= y_offset + mosaiced_h:
-                # 모자이크 영역에 속하는 바운딩 박스의 좌표를 모자이크 영역 내부로 이동
-                box[0] = max(box[0] - x_offset, 0)
-                box[1] = max(box[1] - y_offset, 0)
-                box[2] = min(box[2] - x_offset, mosaiced_w)
-                box[3] = min(box[3] - y_offset, mosaiced_h)
-                # 모자이크 영역에 속하는 바운딩 박스의 라벨을 모자이크된 이미지에 맞게 업데이트
-                boxes[i] = box
-
-        return new_img, boxes, labels
-
-
 
     def RandomBrightness(self, bgr):
         if random.random() < 0.5:

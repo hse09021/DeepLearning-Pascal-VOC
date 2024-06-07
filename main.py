5008dde82aa435bda9cbe4573780d7f56a0f290e
kimgeonsu <kundoo0412@gmail.com>
2024-06-07 09:16:52 +0900

    final network modify

--- main.py
+++ main.py
@@ -7,17 +7,16 @@ import torch
 import torchvision
 from torchvision import transforms
 
-from nets.nn import resnet50, resnet50FPN
+from nets.nn import resnet50, resnet152
 from utils.loss import yoloLoss
 from utils.dataset import Dataset
+
 import argparse
 import re
 
 
 def main(args):
-    # CUDA를 사용하여 장치 설정
-    device = torch.device(
-        'cuda' if torch.cuda.is_available() else 'cpu')
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     root = args.data_dir
     num_epochs = args.epoch
@@ -29,7 +28,7 @@ def main(args):
     torch.manual_seed(seed)
 
     # net = resnet50()
-    net = resnet50FPN()
+    net = resnet152()
 
     if (args.pre_weights != None):
         pattern = 'yolov1_([0-9]+)'
@@ -37,9 +36,8 @@ def main(args):
         f_name = strs.split('/')[-1]
         epoch_str = re.search(pattern, f_name).group(1)
         epoch_start = int(epoch_str) + 1
-        state_dict = torch.load(
-            f'./weights/{args.pre_weights}', map_location=device)['state_dict']
-        net.load_state_dict(state_dict, strict=False)
+        net.load_state_dict(
+            torch.load(f'./weights/{args.pre_weights}')['state_dict'])
     else:
         epoch_start = 1
         resnet = torchvision.models.resnet50(pretrained=True)
@@ -72,37 +70,38 @@ def main(args):
         else:
             params += [{'params': [value], 'lr': learning_rate}]
 
-    optimizer = torch.optim.SGD(
-        params, lr=learning_rate, momentum=0.9, weight_decay=5e-4)
-
-    # optimizer = torch.optim.SGD(net.parameters(), lr=learning_rate, momentum=0.9, weight_decay=5e-4)
+    optimizer = torch.optim.Adamax(params, lr=learning_rate, weight_decay=5e-4)
 
     with open('./Dataset/train.txt') as f:
         train_names = f.readlines()
     train_dataset = Dataset(root, train_names, train=True,
-                            transform=[])
+                            transform=[transforms.ToTensor()])
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True,
-                                               num_workers=0)
+                                               num_workers=os.cpu_count())
 
     with open('./Dataset/test.txt') as f:
         test_names = f.readlines()
     test_dataset = Dataset(root, test_names, train=False,
-                           transform=[])
+                           transform=[transforms.ToTensor()])
     test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size // 2, shuffle=False,
-                                              num_workers=0)
+                                              num_workers=os.cpu_count())
 
     print(f'NUMBER OF DATA SAMPLES: {len(train_dataset)}')
     print(f'BATCH SIZE: {batch_size}')
 
+    best_validation_loss = float("inf")
+    patience = 5
+    count = 0
+
     for epoch in range(epoch_start, num_epochs):
         net.train()
 
-        if epoch == 30:
-            learning_rate = 0.0001
-        if epoch == 40:
-            learning_rate = 0.00001
-        for param_group in optimizer.param_groups:
-            param_group['lr'] = learning_rate
+        # if epoch == 30:
+        #     learning_rate = 0.0001
+        # if epoch == 40:
+        #     learning_rate = 0.00001
+        # for param_group in optimizer.param_groups:
+        #     param_group['lr'] = learning_rate
 
         # training
         total_loss = 0.
@@ -110,60 +109,19 @@ def main(args):
         progress_bar = tqdm.tqdm(
             enumerate(train_loader), total=len(train_loader))
         for i, (images, target) in progress_bar:
-            images = images.to(device)  # (8, 448, 448, 3)
-            images = images.permute(0, 3, 1, 2)
-            target = target.to(device)  # (8, 14, 14, 30)
-            # target 텐서에서 boxes와 labels를 추출하는 코드
-            grid_size = 14
-            target_shape = target.shape  # (batch_size, 14, 14, 30)
-            batch_size = target_shape[0]
-            cell_size = 1. / grid_size
+            images = images.to(device)
+            target = target.to(device)
 
-            target_dict = []
-            for b in range(batch_size):
-                dd = {'boxes': [], 'labels': []}
-                for i in range(grid_size):
-                    for j in range(grid_size):
-                        cell = target[b, i, j]
-                        # confidence가 있는 cell만 처리
-                        if cell[4] > 0.5 or cell[9] > 0.5:
-                            # 두 개의 bounding box 처리
-                            for b_idx in [0, 5]:
-                                box_confidence = cell[4 + b_idx]
-                                if box_confidence > 0.5:
-                                    # 클래스 확률이 가장 높은 클래스 찾기
-                                    class_probabilities = cell[10:]
-                                    class_label = torch.argmax(
-                                        class_probabilities)
-                                    dd['labels'].append(class_label.item())
+            pred = net(images)
 
-                                    # bounding box 정보 추출
-                                    cx = (j + cell[b_idx]) * cell_size
-                                    cy = (i + cell[b_idx + 1]) * cell_size
-                                    width = cell[b_idx + 2] * cell_size
-                                    height = cell[b_idx + 3] * cell_size
-                                    x1 = (cx - width / 2) * target_shape[2]
-                                    y1 = (cy - height / 2) * target_shape[3]
-                                    x2 = (cx + width / 2) * target_shape[2]
-                                    y2 = (cy + height / 2) * target_shape[3]
-                                    dd['boxes'].append([x1, y1, x2, y2])
-
-                dd['boxes'] = torch.tensor(dd['boxes']).to(device)
-                dd['labels'] = torch.tensor(dd['labels']).to(device)
-                target_dict.append(dd)
-
-            target_dict = np.array(target_dict)
-            pred = net(images, target_dict)
             optimizer.zero_grad()
-            # loss = criterion(pred, target.float())
+            loss = criterion(pred, target.float())
 
-            # loss.backward()
+            loss.backward()
             optimizer.step()
 
-            # total_loss += loss.item()
-            # total_loss += pred.item('loss_objectness')
-            total_loss = 0
-            mem = '%.3gG' % (torch.cuda.memory_reserved(0) /
+            total_loss += loss.item()
+            mem = '%.3gG' % (torch.cuda.memory_reserved() /
                              1E9 if torch.cuda.is_available() else 0)
             s = ('%10s' + '%10.4g' + '%10s') % ('%g/%g' %
                                                 (epoch, num_epochs), total_loss / (i + 1), mem)
@@ -176,59 +134,25 @@ def main(args):
             progress_bar = tqdm.tqdm(
                 enumerate(test_loader), total=len(test_loader))
             for i, (images, target) in progress_bar:
-                images = images.to(device)  # (8, 448, 448, 3)
-                images = images.permute(0, 3, 1, 2)
-                target = target.to(device)  # (8, 14, 14, 30)
-                # target 텐서에서 boxes와 labels를 추출하는 코드
-                grid_size = 14
-                target_shape = target.shape  # (batch_size, 14, 14, 30)
-                batch_size = target_shape[0]
-                cell_size = 1. / grid_size
+                images = images.to(device)
+                target = target.to(device)
 
-                target_dict = []
-                for b in range(batch_size):
-                    dd = {'boxes': [], 'labels': []}
-                    for i in range(grid_size):
-                        for j in range(grid_size):
-                            cell = target[b, i, j]
-                            # confidence가 있는 cell만 처리
-                            if cell[4] > 0.5 or cell[9] > 0.5:
-                                # 두 개의 bounding box 처리
-                                for b_idx in [0, 5]:
-                                    box_confidence = cell[4 + b_idx]
-                                    if box_confidence > 0.5:
-                                        # 클래스 확률이 가장 높은 클래스 찾기
-                                        class_probabilities = cell[10:]
-                                        class_label = torch.argmax(
-                                            class_probabilities)
-                                        dd['labels'].append(class_label.item())
+                prediction = net(images)
+                loss = criterion(prediction, target)
+                validation_loss += loss.data
 
-                                        # bounding box 정보 추출
-                                        cx = (j + cell[b_idx]) * cell_size
-                                        cy = (i + cell[b_idx + 1]) * cell_size
-                                        width = cell[b_idx + 2] * cell_size
-                                        height = cell[b_idx + 3] * cell_size
-                                        x1 = (cx - width / 2) * target_shape[2]
-                                        y1 = (cy - height / 2) * \
-                                            target_shape[3]
-                                        x2 = (cx + width / 2) * target_shape[2]
-                                        y2 = (cy + height / 2) * \
-                                            target_shape[3]
-                                        dd['boxes'].append([x1, y1, x2, y2])
-
-                    dd['boxes'] = torch.tensor(dd['boxes']).to(device)
-                    dd['labels'] = torch.tensor(dd['labels']).to(device)
-                    target_dict.append(dd)
-
-                target_dict = np.array(target_dict)
-                prediction = net(images, target_dict)
-                # loss = criterion(prediction, target)
-                # validation_loss += loss.data
-
-        # validation_loss /= len(test_loader)
-        # validation_loss = prediction.item('loss_objectness')
-        validation_loss = 0
+        validation_loss /= len(test_loader)
         print(f'Validation_Loss:{validation_loss:07.3}')
+
+        """ early stopping 추가 """
+        if validation_loss < best_validation_loss:
+            best_validation_loss = validation_loss
+            count = 0
+        else:
+            count += 1
+            if count >= patience:
+                print(f"Early stopping at epoch={epoch}")
+                break
 
         # if epoch % 5:
         #    save = {'state_dict': net.state_dict()}
@@ -246,7 +170,7 @@ if __name__ == '__main__':
 
     parser.add_argument("--batch_size", type=int, default=8)
     parser.add_argument("--epoch", type=int, default=30)
-    parser.add_argument("--lr", type=float, default=0.001)
+    parser.add_argument("--lr", type=float, default=0.0001)
     parser.add_argument("--data_dir", type=str, default='./Dataset')
     parser.add_argument("--pre_weights", type=str, help="pretrained weight")
     parser.add_argument("--save_dir", type=str, default="./weights")
--- nets/nn.py
+++ nets/nn.py
@@ -4,9 +4,10 @@ import torch.nn as nn
 import math
 import torch.utils.model_zoo as model_zoo
 import torch.nn.functional as F
-from torchvision.models import vit_b_16
+from torchvision.models import resnet50, vit_b_16
 import torchvision.models.detection
 from torchvision.models.detection import FasterRCNN_ResNet50_FPN_Weights
+from torchvision.models.detection.backbone_utils import resnet_fpn_backbone
 
 resnet50_url = 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
 
@@ -88,188 +89,39 @@ class Bottleneck(nn.Module):
         return out
 
 
-class DetNet(nn.Module):
-    # no expansion
-    # dilation = 2
-    # type B use 1x1 conv
-    expansion = 1
-
-    def __init__(self, in_planes, planes, stride=1, block_type='A'):
-        super(DetNet, self).__init__()
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
-                               stride=stride, padding=2, bias=False, dilation=2)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.conv3 = nn.Conv2d(planes, self.expansion *
-                               planes, kernel_size=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(self.expansion * planes)
-
-        self.downsample = nn.Sequential()
-        if stride != 1 or in_planes != self.expansion * planes or block_type == 'B':
-            self.downsample = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion * planes,
-                          kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion * planes)
-            )
-
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = F.relu(self.bn2(self.conv2(out)))
-        out = self.bn3(self.conv3(out))
-        out += self.downsample(x)
-        out = F.relu(out)
-        return out
-
-
-class ResNet(nn.Module):
-    def __init__(self, block, layers, num_classes=1000):
-        self.in_planes = 64
-        super(ResNet, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=7,
-                               stride=2, padding=3, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
+class ResNetFPN(nn.Module):
+    def __init__(self, num_classes=1000):
+        super(ResNetFPN, self).__init__()
+        self.backbone = resnet_fpn_backbone('resnet50', pretrained=True)
+        self.fpn = self.backbone.fpn
         self.relu = nn.ReLU(inplace=True)
-
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-
-        self.layer1 = self._make_layer(block, 64, layers[0])
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
-        # self.layer5 = self._make_layer(block, 512, layers[3], stride=2)
-        self.layer5 = self._make_detnet_layer(in_channels=2048)
-        # self.avgpool = nn.AvgPool2d(14) #fit 448 input size
-        # self.fc = nn.Linear(512 * block.expansion, num_classes)
         self.conv_end = nn.Conv2d(
-            256, 30, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn_end = nn.BatchNorm2d(30)
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, math.sqrt(2. / n))
-            elif isinstance(m, nn.BatchNorm2d):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
-
-    def _make_layer(self, block, planes, blocks, stride=1):
-        downsample = None
-        if stride != 1 or self.in_planes != planes * block.expansion:
-            downsample = nn.Sequential(
-                nn.Conv2d(self.in_planes, planes * block.expansion,
-                          kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(planes * block.expansion),
-            )
-        # [3, 4, 6, 3]
-        layers = [block(self.in_planes, planes, stride, downsample)]
-        self.in_planes = planes * block.expansion
-        for i in range(1, blocks):
-            layers.append(block(self.in_planes, planes))
-
-        return nn.Sequential(*layers)
-
-    def _make_detnet_layer(self, in_channels):
-        layers = [
-            DetNet(in_planes=in_channels, planes=256, block_type='B'),
-            DetNet(in_planes=256, planes=256, block_type='A'),
-            DetNet(in_planes=256, planes=256, block_type='A')
-        ]
-        return nn.Sequential(*layers)
+            256, num_classes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn_end = nn.BatchNorm2d(num_classes)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
+        x = self.backbone(x)['0']  # Extracting the first feature map from FPN
         x = self.relu(x)
-        x = self.maxpool(x)
-
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-        x = self.layer5(x)
-        # x = self.avgpool(x)
-        # x = x.view(x.size(0), -1)
-        # x = self.fc(x)
         x = self.conv_end(x)
         x = self.bn_end(x)
         x = torch.sigmoid(x)
-        # x = x.view(-1,14,14,30)
-        x = x.permute(0, 2, 3, 1)  # (-1,14,14,30)
+        x = x.permute(0, 2, 3, 1)  # Change the channel to the last dimension
 
         return x
 
 
 # resnet50
 def resnet50(pretrained=False, **kwargs):
-    model_ = ResNet(Bottleneck, [3, 4, 6, 3], **kwargs)
-    if pretrained:
-        model_.load_state_dict(model_zoo.load_url(
-            'https://download.pytorch.org/models/resnet50-19c8e357.pth'))
+    model_ = ResNetFPN(**kwargs)
     return model_
 
 
 def resnet152(pretrained=False, **kwargs):
-    model_ = ResNet(Bottleneck, [3, 4, 6, 3], **kwargs)
-    if pretrained:
-        model_.load_state_dict(model_zoo.load_url(
-            'https://download.pytorch.org/models/resnet152-394f9c45.pth'))
-    return model_
-
-
-def vit(pretrained=True, **kwargs):
-    model_ = vit_b_16(pretrained=pretrained, **kwargs)
-
-    return model_
-
-
-def get_model_instance_segmentation(num_classes):
-    from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
-
-    # COCO 에서 미리 학습된 인스턴스 분할 모델을 읽어옵니다
-    model = torchvision.models.detection.maskrcnn_resnet50_fpn(
-        weights="DEFAULT")
-
-    # 분류를 위한 입력 특징 차원을 얻습니다
-    in_features = model.roi_heads.box_predictor.cls_score.in_features
-    # 미리 학습된 헤더를 새로운 것으로 바꿉니다
-    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
-
-    # 마스크 분류기를 위한 입력 특징들의 차원을 얻습니다
-    in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
-    hidden_layer = 256
-    # 마스크 예측기를 새로운 것으로 바꿉니다
-    model.roi_heads.mask_predictor = MaskRCNNPredictor(in_features_mask,
-                                                       hidden_layer,
-                                                       num_classes)
-
-    return model
-
-
-def resnet50FPN(pretrained=True, **kwargs):
-    from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
-    from torchvision.models.detection.rpn import AnchorGenerator
-    from torchvision.models.detection import FasterRCNN
-
-    model_ = torchvision.models.detection.fasterrcnn_resnet50_fpn(
-        weights=FasterRCNN_ResNet50_FPN_Weights.DEFAULT, **kwargs)
-    in_features = model_.roi_heads.box_predictor.cls_score.in_features
-    model_.roi_heads.box_predictor = FastRCNNPredictor(in_features, 30)
-    backbone = torchvision.models.mobilenet_v2(weights="DEFAULT").features
-    backbone.out_channels = 1280
-    anchor_generator = AnchorGenerator(sizes=((32, 64, 128, 256, 512),),
-                                       aspect_ratios=((0.5, 1.0, 2.0),))
-    roi_pooler = torchvision.ops.MultiScaleRoIAlign(featmap_names=['0'],
-                                                    output_size=7,
-                                                    sampling_ratio=2)
-    model_ = FasterRCNN(backbone,
-                        num_classes=30,
-                        rpn_anchor_generator=anchor_generator,
-                        box_roi_pool=roi_pooler)
+    model_ = ResNetFPN(**kwargs)
     return model_
 
 
 if __name__ == '__main__':
     a = torch.randn((2, 3, 448, 448))
-    # model = resnet50()
-    model = resnet50FPN()
+    model = resnet50()
     print(model(a))
